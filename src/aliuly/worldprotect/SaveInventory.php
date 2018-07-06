9cc03d6fcc0be8261e19b709d24b6b437442cace
jazzwhistle <jdcraftmcpe@gmail.com>
2018-07-06 17:05:28 +0800

    Restored 'death-save-inv' config option; defaults to true to preserve previous behaviour

--- src/aliuly/worldprotect/SaveInventory.php
+++ src/aliuly/worldprotect/SaveInventory.php
@@ -23,9 +23,11 @@ use pocketmine\Server;
 class SaveInventory extends BaseWp implements Listener {
 	const TICKS = 10;
 	const DEBUG = false;
+	private $saveOnDeath = true;
 
 	public function __construct(Plugin $plugin) {
 		parent::__construct($plugin);
+		$this->saveOnDeath = $plugin->getConfig()->getNested("features")["death-save-inv"] ?? true;
 		Server::getInstance()->getPluginManager()->registerEvents($this, $this->owner);
 	}
 
@@ -76,6 +78,7 @@ class SaveInventory extends BaseWp implements Listener {
 	}
 
 	public function PlayerDeath(PlayerDeathEvent $event) {
+		if(!$this->saveOnDeath) return;
 		$player = $event->getPlayer();
 		// Need to restore inventory (but later!).
 		$this->owner->getScheduler()->scheduleDelayedTask(new PluginCallbackTask($this->owner, [$this, "loadInv"], [$player]), self::TICKS);
